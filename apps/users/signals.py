76ee1d19bce288bc59cfcf4f89dea06d3262af4f
dv-markov <dmitry.v.markov@gmail.com>
2023-05-21 23:01:14 +0300

    added modified signal to update last_active time of UserProfile

--- apps/users/signals.py
+++ apps/users/signals.py
@@ -17,12 +17,9 @@ def create_user_profile(sender, instance, created, **kwargs):
             user_profile.save()
 
 
-# @receiver(pre_save, sender=CustomUser)
-# def update_last_active(sender, instance, **kwargs):
-#     if instance.is_authenticated:
-#         try:
-#             profile = instance.userprofile
-#         except UserProfile.DoesNotExist:
-#             profile = UserProfile(user=instance)
-#         profile.last_active = timezone.now()
-#         profile.save()
+@receiver(pre_save, sender=CustomUser)
+def update_last_active(sender, instance, **kwargs):
+    if instance.is_authenticated:
+        profile = UserProfile.objects.get(user=instance)
+        profile.last_active = timezone.now()
+        profile.save()
