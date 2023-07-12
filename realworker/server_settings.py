6cbdae647c5d2dd523191eebba781108c47d7010
dv-markov <dmitry.v.markov@gmail.com>
2023-07-12 07:49:21 +0300

    add 'http://192.168.9.159' to trusted origins

--- realworker/server_settings.py
+++ realworker/server_settings.py
@@ -133,7 +133,8 @@ STATICFILES_DIRS = []
 CSRF_TRUSTED_ORIGINS = ['http://78.107.195.24',
                         'http://78.107.195.24/admin',
                         'http://realworker.ru',
-                        'http://realworker.ru/admin'
+                        'http://realworker.ru/admin',
+                        'http://192.168.9.159',
                         ]
 
 # User
@@ -144,6 +145,7 @@ AUTH_USER_MODEL = 'users.CustomUser'
 # CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
+    'http://192.168.9.159',
 ]  # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
 # CORS_ALLOWED_ORIGIN_REGEXES = [
 #     'http://localhost:3030',
